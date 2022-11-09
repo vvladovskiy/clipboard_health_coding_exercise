60ffd717b62bea093b1a533fbb99f51f3e2fb485
Vick <vvladovskiy@gmail.com>
2022-11-08 17:18:02 -0800

    This is my Project now!

---
+++ src/test/java/clipboard_health_coding_exercise/runner/CukesRunner.java
@@ -0,0 +1,21 @@
+package clipboard_health_coding_exercise.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt" ,
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features" ,
+        glue = "com/cydeo/step_definitions",
+        dryRun = true,
+        tags = "@webAppLogin"
+)
+public class CukesRunner {
+}
